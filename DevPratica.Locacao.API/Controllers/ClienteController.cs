fc2eb5c23ce512c7b0eb5ba8522eb89d463d24c8
Guilherme <Guilherme@DESKTOP-O0IMRAT>
2023-07-11 19:36:55 -0300

    Ajuste email

--- DevPratica.Locacao.API/Controllers/ClienteController.cs
+++ DevPratica.Locacao.API/Controllers/ClienteController.cs
@@ -2,6 +2,9 @@
 using DevPratica.Locacao.Negocio.ClienteNegocio;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
 
 namespace DevPratica.Locacao.API.Controllers
 {
@@ -22,6 +25,10 @@ namespace DevPratica.Locacao.API.Controllers
             if (ModelState.IsValid)
             {
                 await _clienteNegocio.Incluir(cliente);
+
+                if(cliente.Email != null)
+                    await EnviarEmail(cliente.Email, cliente.Nome);
+
             }
         }
 
@@ -42,6 +49,36 @@ namespace DevPratica.Locacao.API.Controllers
         {
             if (ModelState.IsValid)
                 await _clienteNegocio.Alterar(cliente);      
+        }
+
+        private async Task EnviarEmail(string email, string nome)
+        {
+            MailMessage mensagem = new MailMessage();
+            mensagem.From = new MailAddress("turma2@devpratica.com.br");
+            mensagem.To.Add(email);
+            mensagem.Subject = "Bem=Vindo!!!";
+            mensagem.IsBodyHtml = true;
+            mensagem.Body = EmailBoasVindas(nome);
+
+            var smptCliente = new SmtpClient("smtp.kinghost.net")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("turma2@devpratica.com.br", "Senha@senha10"),
+                EnableSsl = false,
+            };
+
+            smptCliente.Send(mensagem);
+        }
+
+        private string EmailBoasVindas(string nome)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"<p>Parabéns <b>{nome},</b></p>");
+            sb.Append($"<p>Seja muito bem-vindo a <b>G&R Locação.</b></p>");
+            sb.Append($"<p>Estamos muito felizes de você fazer parte da <b>G&R Locação</b></p>");
+            sb.Append($"<br>");
+            sb.Append($"<p>Grande abraço</p>");
+            return sb.ToString();
         }
     }
 }
